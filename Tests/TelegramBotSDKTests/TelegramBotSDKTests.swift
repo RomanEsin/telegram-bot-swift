c35d6a68503bbeb3d17a7fdcd5d190e8c8bc47d8
Roman Esin <esinromanswift@gmail.com>
2021-07-22 20:15:21 +1000

    Changed test bot token and added send image test case

--- Tests/TelegramBotSDKTests/TelegramBotSDKTests.swift
+++ Tests/TelegramBotSDKTests/TelegramBotSDKTests.swift
@@ -15,9 +15,9 @@ class TelegramBotTests: XCTestCase {
         print("[TEST LOGGER]: \($0 ?? "nil")")
     }
 
-    let botToken = "1902701325:AAHvOTL-RqJDo8gLtImcaot-Hzu7bh9lo6o"
+    let botToken = "1916636333:AAHDzlsi8MGp8CVjUYSn16fUA3YizTw1RKg"
 
-    func testGetBot() {
+    func testGetBot() throws {
         let bot = TelegramBot(token: botToken, fetchBotInfo: false)
 
         guard let botUser: User = bot.requestSync("getMe") else {
@@ -25,14 +25,34 @@ class TelegramBotTests: XCTestCase {
             return
         }
 
-        XCTAssertEqual(botUser.username, "asap_feedback_dev_bot")
+        XCTAssertEqual(botUser.username, "asap_feedback_local_bot")
     }
 
-    func testSendMessage() {
+    func testSendMessage() throws {
         let bot = TelegramBot(token: botToken)
         let msg = bot.sendMessageSync(chatId: .chat(440740323), text: "Hello, world!")
         XCTAssertNotNil(msg)
     }
+
+    func testSendImage() throws {
+        let bot = TelegramBot(token: botToken)
+        
+    }
+
+    // Finish this test later
+//    func testGetUpdates() throws {
+//        let bot = TelegramBot(token: botToken)
+//        let router = Router(bot: bot)
+//
+//        let msg = bot.sendMessageSync(chatId: .chat(440740323), text: "Starting bot tests!")
+//        XCTAssertNotNil(msg)
+//
+//        if let update = bot.nextUpdateSync() {
+//            try router.process(update: update)
+//        } else {
+//            XCTAssert(false, "Bot update is nil")
+//        }
+//    }
 }
 
 #if os(Linux)
